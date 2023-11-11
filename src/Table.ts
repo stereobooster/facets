79049f739bea7901da81df03538390560a3ab4a8
stereobooster <stereobooster@gmail.com>
2023-11-11 21:17:12 +0100

    trying something

--- src/Table.ts
+++ src/Table.ts
@@ -8,6 +8,32 @@ type Facet = {
   sort?: string[];
 };
 
+type Filter = OrFilter | AndFilter | EqFilter | NotFilter;
+type EqFilter = {
+  op: "eq";
+  column: string;
+  value: string | number;
+};
+type OrFilter = {
+  op: "or";
+  val: [Filter, Filter];
+};
+type AndFilter = {
+  op: "and";
+  val: [Filter, Filter];
+};
+type NotFilter = {
+  op: "not";
+  val: Filter;
+};
+
+const difference = (a: SparseTypedFastBitSet, b: SparseTypedFastBitSet) =>
+  a.new_difference(b);
+const union = (a: SparseTypedFastBitSet, b: SparseTypedFastBitSet) =>
+  a.new_union(b);
+const intersection = (a: SparseTypedFastBitSet, b: SparseTypedFastBitSet) =>
+  a.new_intersection(b);
+
 type TableOptions = {
   textSearch?: {
     searchableFields: string[];
@@ -18,56 +44,87 @@ type TableOptions = {
 };
 
 export class Table {
-  items: any[];
-  universe: SparseTypedFastBitSet;
-  indexes: Record<string, InvertedIndex>;
-  options: TableOptions;
-  textIndex: InstanceType<TextIndex>;
+  #items: any[];
+  #universe: SparseTypedFastBitSet;
+  #indexes: Record<string, InvertedIndex>;
+  #options: TableOptions;
+  #textIndex: InstanceType<TextIndex>;
 
   constructor(options: TableOptions, items: any[] = []) {
-    this.items = items;
-    this.options = options;
-    this.buildIndex();
+    this.#options = options;
+    this.update(items);
   }
 
-  replace(data: any[]) {}
+  update(items: any[]) {
+    this.#items = items;
+    this.#buildIndex();
+  }
 
   search(query: string, options?: any) {
-    // filter by inverted index and boolean query
-    // filter by full-text
-    // filter by function
-    //
-  }
-
-  buildIndex() {
-    const textIndexClass = this.options.textSearch?.indexer;
-    if (textIndexClass) {
-      this.textIndex = new textIndexClass(
-        // @ts-ignore
-        this.options.textSearch?.searchableFields
-      );
-      if (textIndexClass.usesAddAll) this.textIndex.addAll(this.items);
+    let result: SparseTypedFastBitSet;
+    let sortArr: Array<number> | undefined;
+    if (this.#textIndex && query) {
+      sortArr = this.#textIndex.search(query, options);
+      result = new SparseTypedFastBitSet(sortArr);
     }
 
-    const idKey = this.options.idKey || "id";
-    this.universe = new SparseTypedFastBitSet();
-    this.indexes = {};
-    // Object.entries(facets).forEach(
-    //   ([key, klass]) => (this.indexes[key] = new klass())
-    // );
-    this.items.forEach((item, i) => {
-      this.universe.add(i);
+    // filter by inverted index and boolean query
+    // #evaluate
+
+    // filter by function
+    // sort
+    // pagination
+    // facets
+  }
+
+  #evaluate(f: Filter): SparseTypedFastBitSet {
+    switch (f.op) {
+      case "eq":
+        return this.#indexes[f.column].get(f.value);
+      case "not":
+        return difference(this.#universe, this.#evaluate(f.val)) as any;
+      case "or":
+        return union(this.#evaluate(f.val[0]), this.#evaluate(f.val[1]));
+      case "and":
+        return intersection(
+          this.#evaluate(f.val[0]),
+          this.#evaluate(f.val[1])
+        ) as any;
+    }
+  }
+
+  #buildIndex() {
+    const textIndexClass = this.#options.textSearch?.indexer;
+    const facets = this.#options.facets || {};
+    if (textIndexClass) {
+      this.#textIndex = new textIndexClass(
+        // @ts-ignore
+        this.#options.textSearch?.searchableFields
+      );
+    }
+
+    const idKey = this.#options.idKey || "id";
+    this.#universe = new SparseTypedFastBitSet();
+    this.#indexes = {};
+    Object.keys(facets).forEach(
+      (key) => (this.#indexes[key] = new facets[key].indexer())
+    );
+    this.#items.forEach((item, i) => {
+      this.#universe.add(i);
       if (textIndexClass?.requiresId) item[idKey] = i;
-      if (textIndexClass?.usesAddOne) this.textIndex.addOne(i, item);
-      // Object.keys(facets).forEach((key) => {
-      //   const index = indexes[key];
-      //   const value = item[key];
-      //   if (Array.isArray(value)) {
-      //     value.forEach((v) => index.add(v, i));
-      //   } else {
-      //     index.add(value, i);
-      //   }
-      // });
+      if (textIndexClass?.usesAddOne) this.#textIndex.addOne(i, item);
+      Object.keys(facets).forEach((key) => {
+        const index = this.#indexes[key];
+        const value = item[key];
+        if (Array.isArray(value)) {
+          value.forEach((v) => index.add(v, i));
+        } else {
+          index.add(value, i);
+        }
+      });
     });
+
+    // after adding ids to items
+    if (textIndexClass?.usesAddAll) this.#textIndex.addAll(this.#items);
   }
 }
