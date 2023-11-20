5b4ba9958ba894021f3908a72bcd5bc861e47792
stereobooster <stereobooster@gmail.com>
2023-11-20 15:08:54 +0100

    last touches

--- packages/facets/src/ITrieMapIndex.ts
+++ packages/facets/src/ITrieMapIndex.ts
@@ -1,18 +1,18 @@
-import TrieMap from "mnemonist/trie-map.js";
-import { SparseTypedFastBitSet } from "typedfastbitset";
-import { InvertedIndexMaplike } from "./InvertedIndex";
+// import TrieMap from "mnemonist/trie-map.js";
+// import { SparseTypedFastBitSet } from "typedfastbitset";
+// import { InvertedIndexMaplike } from "./InvertedIndex";
 
-export class ITrieMapIndex<K> extends InvertedIndexMaplike<K> {
-  index: TrieMap<K, SparseTypedFastBitSet>;
+// export class ITrieMapIndex<K> extends InvertedIndexMaplike<K> {
+//   index: TrieMap<K, SparseTypedFastBitSet>;
 
-  constructor() {
-    super();
-    this.index = new TrieMap();
-  }
+//   constructor() {
+//     super();
+//     this.index = new TrieMap();
+//   }
 
-  like(prefix: K) {
-    return this.index
-      .find(prefix)
-      .reduce((p, [_, v]) => p.union(v), new SparseTypedFastBitSet());
-  }
-}
+//   like(prefix: K) {
+//     return this.index
+//       .find(prefix)
+//       .reduce((p, [_, v]) => p.union(v), new SparseTypedFastBitSet());
+//   }
+// }
